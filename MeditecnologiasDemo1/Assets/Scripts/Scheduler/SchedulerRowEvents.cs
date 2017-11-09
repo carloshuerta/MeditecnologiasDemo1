ea9842dd3d6fd2205f7aee8237a6e564bff3d884
Sebastian Gambolati <sgambolati@gmail.com>
2017-11-09 12:22:46 -0300

    Add again ignored files.

---
+++ MeditecnologiasDemo1/Assets/Scripts/Scheduler/SchedulerRowEvents.cs
@@ -0,0 +1,22 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class SchedulerRowEvents : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Row clicked");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
