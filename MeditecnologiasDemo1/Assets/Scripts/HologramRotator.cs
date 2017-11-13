67479248424c5bca958cc287d29145f8cb4132b6
Sebastian Gambolati <sgambolati@gmail.com>
2017-11-13 09:46:10 -0300

    Add Scene to view torax hologram.

---
+++ MeditecnologiasDemo1/Assets/Scripts/HologramRotator.cs
@@ -0,0 +1,72 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class HologramRotator : MonoBehaviour, IInputClickHandler, IHoldHandler
+{
+    private bool isHolding;
+
+    public Transform HologramToRotate;
+    public int RotationSpeed = 50;
+    public RotationDirectionEnum RotationDirection;
+
+    public void OnHoldCanceled(HoldEventData eventData)
+    {
+        this.isHolding = false;
+    }
+
+    public void OnHoldCompleted(HoldEventData eventData)
+    {
+        this.isHolding = false;
+    }
+
+    public void OnHoldStarted(HoldEventData eventData)
+    {
+        this.isHolding = true;
+    }
+
+    private void Update()
+    {
+        if (this.isHolding)
+        {
+            Rotate(this.RotationSpeed * 2);
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Rotate(this.RotationSpeed);
+    }
+
+    private void Rotate(int speed)
+    {
+        Vector3 direction = Vector3.zero;
+
+        switch (this.RotationDirection)
+        {
+            case RotationDirectionEnum.Up:
+                direction = Vector3.left;
+                break;
+            case RotationDirectionEnum.Down:
+                direction = Vector3.right;
+                break;
+            case RotationDirectionEnum.Right:
+                direction = Vector3.down;
+                break;
+            case RotationDirectionEnum.Left:
+                direction = Vector3.up;
+                break;
+            default:
+                break;
+        }
+
+        this.HologramToRotate.Rotate(direction, Time.deltaTime * speed);
+    }
+}
+
+public enum RotationDirectionEnum
+{
+    Up,
+    Down,
+    Right,
+    Left
+}
---
+++ MeditecnologiasDemo1/Assets/Scripts/HologramZoom.cs
@@ -0,0 +1,57 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class HologramZoom : MonoBehaviour, IInputClickHandler, IHoldHandler
+{
+    private bool isHolding;
+    public Transform HologramToZoom;
+    public float ZoomSpeed = 0.1f;
+    public ZoomDirectionEnum ZoomDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isHolding)
+        {
+            MakeZoom();
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        MakeZoom();
+    }
+
+    private void MakeZoom()
+    {
+        if (this.ZoomDirection == ZoomDirectionEnum.In)
+        {
+            HologramToZoom.localScale += new Vector3(ZoomSpeed, ZoomSpeed, ZoomSpeed);
+        }
+        else
+        {
+            HologramToZoom.localScale -= new Vector3(ZoomSpeed, ZoomSpeed, ZoomSpeed);
+        }
+    }
+
+    public void OnHoldStarted(HoldEventData eventData)
+    {
+        this.isHolding = true;
+    }
+
+    public void OnHoldCompleted(HoldEventData eventData)
+    {
+        this.isHolding = false;
+    }
+
+    public void OnHoldCanceled(HoldEventData eventData)
+    {
+        this.isHolding = false;
+    }
+}
+
+public enum ZoomDirectionEnum
+{
+    In,
+    Out
+}
